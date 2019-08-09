f84ecf65f01602c045d5aa3e877484b459c268aa
koudjo AMETEPE <kametepe@pca.ma>
2019-08-09 15:07:46 +0100

    add Docker File

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -19,6 +19,7 @@ namespace Catalog
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://0.0.0.0:80")
                 .UseStartup<Startup>();
     }
 }
