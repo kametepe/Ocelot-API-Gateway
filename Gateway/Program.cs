4c5cddee7cb0433ba563c32260c3687ab31ec157
koudjo AMETEPE <kametepe@pca.ma>
2019-08-09 11:40:38 +0100

    Configure Ocelot to server as API Gateway

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
 
 namespace Gateway
 {
@@ -14,11 +10,32 @@ namespace Gateway
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseStartup<Startup>()
+                          .ConfigureAppConfiguration((hostingContext, config) =>
+                          {
+                              config.AddJsonFile("ocelot.json")
+                              .AddEnvironmentVariables();
+                          })
+                          .ConfigureServices(s =>
+                          {
+                              s.AddOcelot();
+                          })
+                          .Configure(app =>
+                          {
+                              app.UseOcelot().Wait();
+                          })
+                          .ConfigureLogging((hostingContext, logging) =>
+                          {
+                              //add your logging
+                          })
+                          .UseIISIntegration()
+                          .Build();
+        }
     }
 }
